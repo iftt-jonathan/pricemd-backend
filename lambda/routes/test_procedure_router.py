1e8ecd3435535ff388d565f456d4ed7c70957ce6
rebek <math919191@gmail.com>
2025-02-28 19:14:06 -0700

    real test

--- lambda/routes/test_procedure_router.py
+++ lambda/routes/test_procedure_router.py
@@ -1,6 +1,25 @@
-# from procedure_router import get_handler, search_handler 
+from procedure_router import get_handler, search_handler 
+import json
 
-def test_procedure_router():
-    print("hello world from test procedure router") 
-    assert True
+def test_search_handler():
+    result = search_handler("test", "test")
+    assert result["statusCode"] == 200
+    assert result["body"] is not None
 
+    body =  json.loads(result["body"])
+    first_result = body[0]
+    
+    assert first_result["procedure_id"] == 3
+    assert first_result["procedure_name"] == "Tonsil Tumor Removal"
+
+
+def test_get_handler():
+    result = get_handler("test", "test")
+
+    assert result["body"] is not None
+    assert result["statusCode"] == 200
+
+    body =  json.loads(result["body"])
+    assert body["procedure_name"] == "Tonsil Tumor Removal"
+    assert body["insurance_id"] == 4
+    assert body["hospitals"] == [{"hospital_id": 66, "price": 55.00}]
