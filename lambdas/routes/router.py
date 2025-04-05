a1698fcebe17487406ed3162fe74dd673c28e392
Jonathan Ballard <jonballard6@outlook.com>
2025-04-05 13:50:59 -0600

    added get_handler back for easier verification with curl, updated main.tf to use the renamed routes

--- lambdas/routes/router.py
+++ lambdas/routes/router.py
@@ -6,3 +6,15 @@ def search_handler(event, context):
     print("querying for: ", name)
 
     return query_handler(context, query)
+
+def get_handler(event, context):
+    procedure_id = event['pathParameters']['procedureId']
+    insurance_id = event['pathParameters']['insuranceId']
+
+    # query = f"SELECT * FROM hospitals.dummy_data WHERE procedure = '{procedure_id}' AND insurance = '{insurance_id}';"
+    # print(f"Querying for procedure: {procedure_id}, insurance: {insurance_id}")
+
+    query = f"SELECT * FROM hospitals.dummy_data WHERE procedure = '{procedure_id}';"
+    print(f"Querying for procedure: {procedure_id}")
+
+    return query_handler(context, query)
